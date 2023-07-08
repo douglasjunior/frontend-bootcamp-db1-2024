75634bf324ff6cf9597067f72cab36f46c437d6b
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2023-07-08 14:51:36 -0300

    Criado variável de ambiente para configurar a URL do backend

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import LocalStorageHelper from './helpers/localstorage-helper';
 
-axios.defaults.baseURL = 'https://backend.bootcamp.douglasjunior.xyz';
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 axios.interceptors.request.use((request) => {
   const token = LocalStorageHelper.getToken();
